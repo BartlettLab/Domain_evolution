33e09b8fe9a42273a919fdafada81bd77c103808
Jarrett Man [jaman@umass.edu] <jm33a@ghpcc06.umassrc.org>
2020-05-12 10:13:26 -0400

    updated initial search to first threshold by hit length

--- 1KP_initial_search_and_tree.sh
+++ 1KP_initial_search_and_tree.sh
@@ -28,20 +28,28 @@ module load R/3.6.1_packages/tidyverse/1.3.0 gcc/8.1.0
 ################################################################################
 #run as bsub < /home/jm33a/domain_evolution/scripts_and_resources/1KP_initial_search_and_tree.sh
 
-
-run=${PWD##*/} #name of current directory without path
-
 scripts_dir=/home/jm33a/domain_evolution/scripts_and_resources
+
+run=${PWD##*/} #name of current directory without path, should be exactly the same as the gene name in the gene ID
+    #set up target gene length and cutoffs
+	target_length=$(grep -A1 AT.*_$run $scripts_dir/scaffold_seqs.fa | tail -1 | wc -m) #get target seq length
+	echo "$run sequence length is $target_length"
+	min_length=$(awk -v target_length="${target_length}" -v percent=".85" 'BEGIN{print (target_length*percent)}' | awk '{print int($1+0.5)}') #get 85% of that length	
+	max_length=$(awk -v target_length="${target_length}" -v percent="1.15" 'BEGIN{print (target_length*percent)}'| awk '{print int($1+0.5)}') #get 115% of that length
+echo "only genes of length between $min_length and $max_length will be collected and used for analysis"
+
 
 
 ##this section will take a list of genes, make an HMM, search all 1kt and n best hit collect sequences, then scan them for domain content
 cd /home/jm33a/domain_evolution/clade_trees/$run/
 
+<<MULTILINE_COMMENT
 echo "constructing database for input sequences, collecting and aligning inputs..."
     cat $(ls ~/supertree/databases/primary_transcript_databases/*.oneline.fa) > combined_onelines.fa #source databases for input genes
     grep -w -A 1 -f *geneIDs.txt --no-group-separator combined_onelines.fa | awk '{print $1}' > $run.input_seqs.fa #collect input clade seqs
     rm combined_onelines.fa
     linsi $run.input_seqs.fa > $run.input_align.fasta
+MULTILINE_COMMENT
 
 echo "make $run hmm from input alignment..."
 	hmmbuild -o hmmout.txt $run.model.hmm $run.input_align.fasta
@@ -49,9 +57,9 @@ echo "make $run hmm from input alignment..."
 
 
 species_to_scan=$(wc -l <  $scripts_dir/flowering_plants_species_IDs) #the number of flowering plant species handles from 1KT
+#species_ID=LSKK
+echo "collect the top 20 hits for each of the $species_to_scan species listed in flowering_plants_species_IDs"
 count=1 #counter for progress
-
-echo "collect the top 2 hits for each of the $species_to_scan species listed in flowering_plants_species_IDs"
 while read species_ID
 do
     echo "Processing species $species_ID ($count out of $species_to_scan)"
@@ -60,19 +68,29 @@ do
    
     test -f $protein_database_path #test if file exists. Write 0 to $? if yes, 1 if no
     if [ $? -eq 0 ] #only perform the search when a database exists, otherwise report the error
-    then
+    then #this bock will collect only sequences that meet length criteria and add the top 2 hits to the top_hits list
         hmmsearch -o hmmout.txt --noali --tblout table.output.txt $run.model.hmm $protein_database_path #scan all sequences from a species with the HMM model
-    
-        #next collec the sequence of the top n hits found in the search
-        for hit_number in {4..5} #top hit is on line 4, specify how many lines after that, this is how many hits to collect from each species
-        do
+        for hit_number in {4..23} #top 20 hits pre-length thresholding
+        do  #check sequence length
             hit=$(sed -n "${hit_number}p" < table.output.txt | awk '{print $1}') #gene ID from hit number line
-            hit_full_ID=">${species_ID}_${hit}" #concatenate the species ID and the geneID with an underscore between, using fasta format >
-            echo $hit_full_ID >> $run.top_hits.seqs.fa #add gene full gene ID to fasta sequences
-            grep -w -A 1 $hit $protein_database_path | tail -n 1 >> $run.top_hits.seqs.fa #add gene's sequence to running list
+            hit_length=$(grep -w -A 1 $hit $protein_database_path | tail -n 1 | wc -m)
+            if [ $hit_length -gt $min_length ]
+            then
+                if [ $hit_length -lt $max_length ]
+                then #add to the temp list if both these criteria are met
+                    hit_full_ID=">${species_ID}_${hit}" #concatenate the species ID and the geneID with an underscore between, using fasta format >
+                    echo $hit_full_ID >> temp_hits.fa #add gene full gene ID to temp doc
+                    fgrep -w -A 1 ">${hit}" $protein_database_path | tail -n 1  >> temp_hits.fa #add gene's sequence to running list
+                fi
+            fi
         done
-    else
-        echo "$species_ID does not have a peptide database"
+ 
+        test -f temp_hits.fa #if any hits were found, add top two hits to the top hits sequences list and reset the temp list
+        if [ $? -eq 0 ]
+        then
+            cat temp_hits.fa | head -4 >> $run.top_hits.seqs.fa
+            rm temp_hits.fa #reset the temp list
+        fi
     fi
     count=`expr $count + 1` #add to count for progress report
 done < $scripts_dir/flowering_plants_species_IDs #the input for flowering plant species handles from 1KT
